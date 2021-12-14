06c8b3b4383b97312c91454168f87a2ab3a5de32
Kai.Chu <chuck.chu.liu@gmail.com>
2021-12-14 11:04:04 +0100

    Prep

---
+++ 2021/src/main/java/Day_17Tasks.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+
+public class Day_17Tasks {
+    private static final AdventOfCode aoc2021 = AOCFactory.aoc2021;
+
+    private static int[] toInt(String[] arr) {
+        return Arrays.stream(arr).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private static void printArr(Object[] arr) {
+        Arrays.stream(arr).forEach(e -> {
+            System.out.print(e + ",");
+        });
+        System.out.println("");
+    }
+
+    @Advent(day = Day.Day_17, part = Part.one)
+    public int Day_17Part1(String[] inputs) throws IOException {
+        return 0;
+    }
+
+    @Advent(day = Day.Day_17, part = Part.two)
+    public int Day_17Part2(String[] inputs) throws IOException {
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        final Platform platform = new Platform();
+        platform.bootstrap(new Day_17Tasks());
+    }
+}
